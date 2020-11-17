b5e1184838a5077ac76e559bcb32982ede247973
Theo <theo@seelye.net>
2020-11-17 00:55:25 -0800

    ITS ALIIIVE

--- src/x86_64/compiler.rs
+++ src/x86_64/compiler.rs
@@ -19,39 +19,44 @@ pub fn to_asm(instr: &Inst) -> String {
             format!("    sub ${}, %r12\n", instr.times)
         },
         InstKind::IncByte => {
-            format!("    addb ${}, r12\n", instr.times)
+            format!("    addb ${}, (%r12)\n", instr.times)
         },
         InstKind::DecByte => {
-            format!("    subb $+{}, r12\n", instr.times)
+            format!("    subb $+{}, (%r12)\n", instr.times)
         },
         InstKind::WriteByte => {
             let mut msg = String::from("    mov $SYS_WRITE, %rax\n");
-            msg.push_str("    mov $STDOUT, %rdi\n");
-            msg.push_str("    mov %r12, %rsi\n");
-            msg.push_str("    mov $1, %rdx\n");
-            for _ in 0..instr.times {
-                let dupe = msg.clone();
-                msg.push_str(&dupe);
+                          msg.push_str("    mov $STDOUT, %rdi\n");
+                          msg.push_str("    mov %r12, %rsi\n");
+                          msg.push_str("    mov $1, %rdx\n");
+                          msg.push_str("    syscall\n");
+            for _ in 0..(instr.times-1) {
+                // If this is repeated, all we have to do is call it more times.
+                // It would be nice if it were possible to just print more bytes,
+                // but making somewhere in memory a string of the given byte and
+                // whatnot would probably be slower than just calling it again?
+                msg.push_str("    syscall\n");
             }
             msg
         },
         InstKind::ReadByte => {
             let mut msg = String::from("    mov $SYS_READ, %rax\n");
-            msg.push_str("    mov $STDIN, %rax\n");
-            msg.push_str("    mov %r12, %rsi\n");
-            msg.push_str("    mov $1, %rdx\n");
+                          msg.push_str("    mov $STDIN, %rax\n");
+                          msg.push_str("    mov %r12, %rsi\n");
+                          msg.push_str("    mov $1, %rdx\n");
+                          msg.push_str("    syscall\n");
             msg
         },
         InstKind::LoopStart {end_idx} => {
-            let mut msg = String::from("    cmp $0, r12\n");
-            msg.push_str(&format!("    je LOOP_END_{}\n", end_idx)[..]);
-            msg.push_str(&format!("LOOP_START_{}:\n", instr.idx)[..]);
+            let mut msg = String::from("    cmpb $0, (%r12)\n");
+                 msg.push_str(&format!("    je LOOP_END_{}\n", end_idx)[..]);
+                 msg.push_str(&format!("LOOP_START_{}:\n", instr.idx)[..]);
             msg
         },
         InstKind::LoopEnd {start_idx} => {
-            let mut msg = String::from("    cmp $0, r12\n");
-            msg.push_str(&format!("    je LOOP_START_{}\n", start_idx)[..]);
-            msg.push_str(&format!("LOOP_END_{}:\n", instr.idx)[..]);
+            let mut msg = String::from("    cmpb $0, (%r12)\n");
+                 msg.push_str(&format!("    jne LOOP_START_{}\n", start_idx)[..]);
+                 msg.push_str(&format!("LOOP_END_{}:\n", instr.idx)[..]);
             msg
         },
     }
